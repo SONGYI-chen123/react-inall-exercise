2612a89f30e911bfe52c5f268b94b72ed704bdc9
SONGYI-chen123 <song1577660501@163.com>
2020-10-08 20:51:00 +0800

    [syc]:counter page rendering

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import '../../style/counter.css'
 
 class Counter extends React.Component{
+    constructor() {
+        super();
+        this.state={
+            num1: 0,
+            num2: 0,
+            result: 0,
+            opera: '',
+
+        }
+    }
     render(){
         return(
-            <div>
-                <p>
-                    This is a counter;
-                </p>
+            <div className="counter">
+                <h1>在线计算器</h1>
+                <div className="counterBody">
+                    <div className="myInput">
+                        <input />
+                    </div>
+                    <div className="myOpera">
+                        <button>+</button>
+                        <button>-</button>
+                        <button>*</button>
+                    </div>
+                    <div className="myNumber">
+                        <button>7</button>
+                        <button>8</button>
+                        <button>9</button>
+                    </div>
+                    <div className="myNumber">
+                        <button>4</button>
+                        <button>5</button>
+                        <button>6</button>
+                    </div>
+                    <div className="myNumber">
+                        <button>1</button>
+                        <button>2</button>
+                        <button>3</button>
+                    </div>
+                    <div className="myNumber">
+                        <button>0</button>
+                        <button id="clearBtn">clear</button>
+                        <button id="equalBtn">=</button>
+                    </div>
+                </div>
             </div>
         );
     }
